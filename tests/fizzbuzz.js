1f68593ee251824913d08bb29abf997cc0192a7c
Lawrence C <lawrencecaudle870@gmail.com>
2024-01-23 21:34:49 -0500

    corrected fizzbuzz function

--- tests/fizzbuzz.js
+++ tests/fizzbuzz.js
@@ -4,7 +4,7 @@ function fizzBuzz(numbers) {
   let result = [];
 
   for (let number of numbers) {
-    if (number % 15 === 0) {
+    if (number % 3 === 0 && number % 5 === 0) {
       result.push("fizzBuzz");
     } else if (number % 3 === 0) {
       result.push("fizz");
